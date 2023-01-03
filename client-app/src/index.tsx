8d2bfa0b4fda465eb3211c67798f63cd42f3e6e8
Felipe-Euzebio <felipe.euzebio.ti@gmail.com>
2023-01-02 22:30:30 -0300

    End of section 11

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,6 +3,7 @@ import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css';
 import 'react-calendar/dist/Calendar.css';
 import 'react-toastify/dist/ReactToastify.min.css';
+import 'react-datepicker/dist/react-datepicker.css';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
 import { RouterProvider } from 'react-router-dom';
